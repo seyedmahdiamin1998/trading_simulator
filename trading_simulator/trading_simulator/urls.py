9fd675178fb60190ea3b6dbf3040082e9942ba4e
seyedmahdiamin <seyedmahdiamin1998@gmail.com>
2023-01-14 19:19:08 +0330

    startapp APIApp

--- trading_simulator/trading_simulator/urls.py
+++ trading_simulator/trading_simulator/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myApp.urls', namespace="myApp")),
+    path('api/', include('APIApp.urls', namespace="API")),
 ]
 
 
